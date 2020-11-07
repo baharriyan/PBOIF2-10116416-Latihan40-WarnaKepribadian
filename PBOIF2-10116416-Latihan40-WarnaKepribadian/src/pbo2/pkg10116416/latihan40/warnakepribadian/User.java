a44f98c32cdf9c35842b7e7b4d78bdb8a1a46e61
baharriyan <indonesiariyan@gmail.com>
2020-11-08 06:00:10 +0700

    first commit

---
+++ PBOIF2-10116416-Latihan40-WarnaKepribadian/src/pbo2/pkg10116416/latihan40/warnakepribadian/User.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116416.latihan40.warnakepribadian;
+
+/**
+ * 
+ * @author  
+ * NAMA     : riyan bahar
+ * KELAS    : IF-2
+ * NIM      : 10116416
+ */
+public class User {
+    private String nama, warna;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+}
